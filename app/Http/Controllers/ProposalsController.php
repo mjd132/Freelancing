2214383c53fc38589168ee200ac86f5f1ce74a8e
Majid Abbasi <majid.abbasi.ce@gmail.com>
2024-06-10 15:47:48 +0330

    Update destroy ,update and accept methods in ProposalsController

--- app/Http/Controllers/ProposalsController.php
+++ app/Http/Controllers/ProposalsController.php
@@ -6,20 +6,19 @@ use App\Enums\ProjectStatus;
 use App\Enums\ProposalStatus;
 use App\Models\Project;
 use App\Models\Proposal;
-use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Http\Request;
 
 class ProposalsController extends Controller
 {
-    use ValidatesRequests;
+
 
     public function create($projectId)
     {
         $project = Project::findOrFail($projectId);
 
-        if ($project->last_status === ProjectStatus::ACCEPTED || $project->last_status === ProjectStatus::DONE) {
-            return to_route('projects.show', $project->id);
-        }
+//        if ($project->last_status === ProjectStatus::ACCEPTED || $project->last_status === ProjectStatus::DONE) {
+//            return to_route('projects.show', $project->id);
+//        }
 
         return view('proposals.create')->with('id', $projectId);
     }
@@ -30,9 +29,9 @@ class ProposalsController extends Controller
         $project = Project::findOrFail($projectId);
 
         // TODO : check user has not registered a proposal before
-        if ($project->status == ProjectStatus::ACCEPTED || $project->state == ProjectStatus::DONE) {
-            return to_route('projects.show', $project->id);
-        }
+//        if ($project->status == ProjectStatus::ACCEPTED || $project->state == ProjectStatus::DONE) {
+//            return to_route('projects.show', $project->id);
+//        }
 
         $this->validate($request, [
             'fId' => 'required|exists:users,id',
@@ -43,55 +42,87 @@ class ProposalsController extends Controller
 
         Proposal::create([
             'freelancer_id' => $request->get('fId'),
-            'project_id' => $pId,
+            'project_id' => $projectId,
             'description' => $request->get('description'),
             'price' => $request->get('price'),
             'delivery_time' => $request->get('delivery_time'),
         ]);
-        return to_route('projects.show', $pId);
+        return to_route('projects.show', $projectId);
     }
 
     public function show($id)
     {
-        $proposal = Proposal::findOrFail($id);
-        return view('proposals.show')->with('proposal', $proposal);
+        $proposals = Proposal::findOrFail($id);
+        return view('proposals.show')->with('proposal', $proposals);
+    }
+
+    public function showAll($projectId)
+    {
+        $proposals = Proposal::where('project_id', $projectId)->latest()->paginate(10);
+        return view('proposals.showAll')->with('proposals', $proposals);
     }
 
     public function edit($id)
     {
-
+        $proposal = Proposal::findOrFail($id);
+        return view('proposals.edit')->with('proposal', $proposal);
     }
 
     public function update(Request $request, $id)
     {
+        $proposal = Proposal::findOrFail($id);
 
+        $this->validate($request, [
+            'fId' => 'required|exists:users,id',
+            'description' => 'required|string',
+            'price' => 'required|numeric|min:1',
+            'delivery_time' => 'required|date|after_or_equal:today',
+        ]);
+
+        $proposal->update([
+            'freelancer_id' => $request->get('fId'),
+            'description' => $request->get('description'),
+            'price' => $request->get('price'),
+            'delivery_time' => $request->get('delivery_time'),
+        ]);
+
+        return to_route('proposals.show', $proposal->id)->with('success', 'Proposal updated successfully.');
     }
 
     public function destroy($id)
     {
-
+        $proposal = Proposal::findOrFail($id);
+        $proposal->delete();
+        return to_route('projects.show', $proposal->project_id)->with('success', 'Proposal deleted successfully.');
     }
 
+    /**
+     *
+     *  Change proposal status to accepted
+     */
     public function accept($id)
     {
         $proposal = Proposal::findOrFail($id);
+        /**policies
+         * if (!$proposal->project->freelancer_id === null) {
+         * return to_route('projects.show', $proposal->project_id);
+         * //check own proposal is not accepted
+         * if ($proposal->isAccepted()) {
+         * return to_route('proposals.show', $proposal->id)->withErrors(['acceptError' => 'Proposal is already accepted']);
+         * }
+         * //check other proposals is not accepted
+         * foreach ($proposal->project->proposals as $prop) {
+         * if ($prop->isAccepted()) {
+         * return to_route('proposals.show', $proposal->id)->withErrors(['acceptError' => 'Other proposal is already accepted']);
+         * }
+         * }
+         * }
+         **/
+        $proposal->update(['status' => ProposalStatus::ACCEPTED->value, 'accepted_at' => now()]);
 
-        if (!$proposal->project->last_status === ProjectStatus::ABANDONED) {
-            // check own proposal is not accepted
-            if ($proposal->isAccepted()) {
-                return to_route('proposals.show', $proposal->id)->withErrors(['acceptError' => 'Proposal is already accepted']);
-            }
-            //check other proposals is not accepted
-            foreach ($proposal->project->proposals as $prop) {
-                if ($prop->isAccepted()) {
-                    return to_route('proposals.show', $proposal->id)->withErrors(['acceptError' => 'Other proposal is already accepted']);
-                }
-            }
-        }
+        $proposal->project->update(['last_status' => ProjectStatus::ACCEPTED->value, 'freelancer_id' => $proposal->freelancer_id]);
 
-        $proposal->update(['status' => ProposalStatus::ACCEPTED, 'accepted_at' => now()]);
-        $proposal->project->update(['last_status' => ProjectStatus::ACCEPTED, 'freelancer_id' => $proposal->freelancer_id]);
-        return to_route('proposals.show', $proposal->id);
+        return to_route('projects.show', $proposal->project_id);
     }
 
 
