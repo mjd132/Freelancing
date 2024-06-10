4ce8c889bd793ad2a4ca878952d2866d01cb7c52
Majid Abbasi <majid.abbasi.ce@gmail.com>
2024-06-10 15:45:18 +0330

    formatting

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,34 +10,43 @@ use Illuminate\Support\Facades\Auth;
 class AuthController extends Controller
 {
     use ValidatesRequests;
-    public function loginPage(Request $request){
+
+    public function loginPage(Request $request)
+    {
         return view('auth.login');
     }
-    public function registerPage(Request $request){
+
+    public function registerPage(Request $request)
+    {
         return view('auth.register');
     }
-    public function login(Request $request){
-        $this->validate($request,[
-            'email'=>'required|email',
-            'password'=>'required|min:5|string'
+
+    public function login(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required|min:5|string'
         ]);
-        if(Auth::attempt($request->only('email','password'))){
+        if (Auth::attempt($request->only('email', 'password'))) {
             return to_route('home');
         }
-        return to_route('login')->withErrors(['error'=>'Email or password invalid']);
+        return to_route('login')->withErrors(['error' => 'Email or password invalid']);
     }
 
-    public  function register(Request $request){
-        $this->validate($request,[
-            'name'=>'required|max:255',
-            'email'=>'required|email|unique:users',
-            'password'=>'required|confirmed|min:5|string'
+    public function register(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed|min:5|string'
         ]);
-        $user = User::create($request->only('name','email','password'));
+        $user = User::create($request->only('name', 'email', 'password'));
         Auth::login($user);
         return to_route('home');
     }
-    public  function logout(Request $request){
+
+    public function logout(Request $request)
+    {
         Auth::logout();
         return to_route('home');
     }
